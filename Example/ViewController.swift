aca9d5a284b6e6373400798c045fc97c938bfbe5
kichikuchi <kikuchi0111@gmail.com>
2017-11-26 23:56:40 +0900

    add example

---
+++ Example/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Kazunori Kikuchi on 2017/11/26.
+//  Copyright © 2017年 kazunori kikuchi. All rights reserved.
+//
+
+import UIKit
+import Exif
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let exif = Exif(forResource: "sample", ofType: "jpg")
+        print(exif.properties)
+    }
+
+}
+
